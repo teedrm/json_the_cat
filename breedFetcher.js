f1d6eca2754812a5a22ee5e9e515f434c788355c
Thy Nguyen <tee@Thys-iMac.local>
2022-11-25 02:51:07 -0700

    cats as a service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = (breedName, callback) => {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log('error', error);
-    return;
-  } else {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+    }
+
     const data = JSON.parse(body);
 
     if (data[0]) {
-      console.log(data[0].description);
+      callback(null, data[0].description);
     } else {
-      console.log('Breed Not Found');
+      callback('Breed Not Found', null);
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
